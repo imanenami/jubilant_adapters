c73e2debe2a9dbe887c525c6642a213b994036cb
Iman Enami <iman.enami@canonical.com>
2026-05-08 14:06:26 +0400

    add adapter tests

--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -1,10 +1,11 @@
 """Basic adapter tests."""
 
 import pytest
-from fixtures import STATUS
+from fixtures import StatusFixtures
 
+from jubilant import Task
 from jubilant_adapters import JujuFixture
-from jubilant_adapters.adapters import LibjujuStatusDict
+from jubilant_adapters.adapters import ActionAdapter, LibjujuStatusDict, MachineAdapter
 
 TEST_MODEL = "testing"
 
@@ -38,5 +39,24 @@ def test_add_secret(juju: JujuFixture, capsys: pytest.CaptureFixture):
 
 
 def test_status_dict():
-    status_dict = LibjujuStatusDict(STATUS["cos"])
+    status_dict = LibjujuStatusDict(StatusFixtures.cos)
     assert "applications" in status_dict
+    assert "traefik/0" in status_dict["applications"]["traefik"]["units"]
+
+
+def test_action():
+    task = Task(id="12", status="completed", results={"done": "true"})
+    action = ActionAdapter(task=task)
+    action.wait()
+    assert action.results["done"] == "true"
+
+
+def test_machine(juju: JujuFixture):
+    status = StatusFixtures.vm
+    juju.status = lambda: status
+    machine = MachineAdapter("2", juju)
+    assert machine.dns_name == "10.148.75.116"
+    assert machine.id == "2"
+    assert machine.agent_status == "started"
+    assert machine.status == "running"
+    assert machine.status_message == "Running"
